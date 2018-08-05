bacd81e3052a9c32ef22e8c0ab66db4c12c5e899
Ahamed <ahamedali95@gmail.com>
2018-08-05 17:16:05 -0400

    displayed dogs details in the table:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const tableBody = document.getElementById("table-body");
 
-})
+  function getDogs() {
+    fetch("http://localhost:3000/dogs")
+    .then(response => response.json())
+    .then(data => showDogs(data));
+  }
+
+  function showDogs(data) {
+    let rows = "<tr>";
+
+    data.forEach(dataObj => {
+      rows += `<tr><td>${dataObj.name}</td> <td>${dataObj.breed}</td> <td>${dataObj.sex}</td> <td><button>Edit</button></td></tr>`
+    });
+
+    rows += "</tr>";
+    tableBody.innerHTML = rows;
+  }
+
+
+
+
+  getDogs();
+});
